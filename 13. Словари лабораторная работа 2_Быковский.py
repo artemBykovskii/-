91851ed7c3abfa2c9d874bf0c2ed3fe0204e5e1d
artemBykovskii <101504327+artemBykovskii@users.noreply.github.com>
2026-02-14 16:00:39 +0800

    Create 13. Словари лабораторная работа 2_Быковский.py

---
+++ 13. Словари лабораторная работа 2_Быковский.py
@@ -0,0 +1,53 @@
+"""zodiac_elements = {"water": ["Cancer", "Scorpion", "Pisces"], "fire": ["Aries",
+                   "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Copricorn"],
+                    "air": ["Germini", "Libra", "Aquarius"]}
+print(zodiac_elements["fire"])"""
+
+
+
+
+"""zodiac_elements = {"water": ["Cancer", "Scorpion", "Pisces"], "fire": ["Aries",
+                   "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Copricorn"],
+                    "air": ["Germini", "Libra", "Aquarius"]}
+zodiac_elements["energy"] = "Not a Zodiac element"
+print(zodiac_elements["energy"])"""
+
+
+
+"""caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+caffeine_level["matcha"] = 30
+try:
+    print(caffeine_level["matcha"])
+except KeyError:
+    print("Неизвестный уровень кофеина")"""
+
+
+
+
+"""user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+tc_id = user_ids.get("teraCoder", 1000)
+print(tc_id)"""
+
+
+
+"""user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+users = user_ids.keys()
+classes = num_exercises.keys()
+print(users)
+print(classes)"""
+
+
+
+
+"""tarot = {1: "The Magician", 2: "The High Priestess", 3: "The Empress", 4: "The Emperor",
+        5: "The Hierophant", 6: "The Lovers", 7: "The Chariot", 8: "Strength",
+        9: "The Hermit", 10: "Wheel of Fortune", 11: "Justice", 12: "The Hanged Man",
+        13: "Death", 14: "Temperance", 15: "The Devil", 16: "The Tower",
+        17: "The Star", 18: "The Moon", 19: "The Sun", 20: "Judgement",
+        21: "The World", 22: "The Fool"}
+spread = {}
+spread["past"] = tarot[13]
+spread["present"] = tarot[22]
+spread["future"] = tarot[10]
+print(spread)"""
